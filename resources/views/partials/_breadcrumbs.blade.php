c2d9aa8a6c6c9392f32d8e7bd0e338b14bd3e4d7
Mark <sitesensedev@gmail.com>
2018-10-06 12:47:48 +0100

    Remove link on 'Home' when current page.
    Minor changes to make it easier to understand and edit for end users.

--- resources/views/partials/_breadcrumbs.blade.php
+++ resources/views/partials/_breadcrumbs.blade.php
@@ -1,18 +1,24 @@
 
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item<?php $breadcrumbs->isEmpty() ? ' active' : '' ?>">
-            <a href="/">Home</a>
-        </li>
-        @foreach($breadcrumbs as $key => $url)
-        @if (! $loop->last)
-        <li class="breadcrumb-item">
-            <a href="{{ url($url) }}">{{ title_case(str_replace('-', ' ', $key)) }}</a>
-        @else
+        @if ($breadcrumbs->isEmpty())
         <li class="breadcrumb-item active" aria-current="page">
-            {{ title_case(str_replace('-', ' ', $key)) }}
+            Home
+        @else
+        <li class="breadcrumb-item">
+            <a href="/">Home</a>
         @endif
         </li>
-    @endforeach
+        @foreach($breadcrumbs as $key => $url)
+        @php $key = title_case(str_replace('-', ' ', $key)) @endphp
+        @if (! $loop->last)
+        <li class="breadcrumb-item">
+            <a href="{{ url($url) }}">{{ $key }}</a>
+        @else
+        <li class="breadcrumb-item active" aria-current="page">
+            {{ $key }}
+        @endif
+        </li>
+        @endforeach
     </ol>
 </nav>
